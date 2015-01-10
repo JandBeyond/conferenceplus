088c9530311c713f7e61589b65ddbfd43a9715dc
Robert Deutz <rdeutz@googlemail.com>
2015-01-10 22:38:11 +0100

    adding tweet bought a ticket

--- code/components/com_conferenceplus/views/payment/tmpl/confirm.php
+++ code/components/com_conferenceplus/views/payment/tmpl/confirm.php
@@ -23,6 +23,10 @@ $doc = JFactory::getDocument()->setTitle($title);
 	<h<?php echo $headerlevel; ?>><?php echo $title; ?></h<?php echo $headerlevel; ?>>
 
 	<?php echo JText::_('COM_CONFERENCEPLUS_TICKET_CONFIRM_BUY_TICKET');?>
+	
+	<br />
+	
+	<a href="https://twitter.com/home?status=I%20just%20bought%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">Share on Twitter with your friends</a>
 
 </div>
 <!-- ************************** END: conferenceplus ************************** -->
