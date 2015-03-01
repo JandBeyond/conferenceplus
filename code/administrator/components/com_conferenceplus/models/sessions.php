ea9f43d2901674eca00692f7535d1b9399c8ebd3
Robert Deutz <rdeutz@googlemail.com>
2015-03-01 18:43:49 +0100

    sessionassignment functionality

--- code/administrator/components/com_conferenceplus/models/sessions.php
+++ code/administrator/components/com_conferenceplus/models/sessions.php
@@ -14,6 +14,11 @@ defined('_JEXEC') or die;
 
 require_once 'default.php';
 
+/**
+ * Class ConferenceplusModelSessions
+ *
+ * @since  0.0
+ */
 class ConferenceplusModelSessions extends ConferenceplusModelDefault
 {
 	/**
@@ -42,6 +47,109 @@ class ConferenceplusModelSessions extends ConferenceplusModelDefault
 	}
 
 	/**
+	 * This method runs after an item has been gotten from the database in a read
+	 * operation. You can modify it before it's returned to the MVC triad for
+	 * further processing.
+	 *
+	 * @param   FOFTable  &$record  The table instance we fetched
+	 *
+	 * @return  void
+	 */
+	protected function onAfterGetItem(&$record)
+	{
+		parent::onAfterGetItem($record);
+
+		$record->assignedRoomsSlots = [];
+
+		if ($this->_isNewRecord)
+		{
+			$event_id = JFactory::getApplication()->getUserState('com_conferenceplus.eventId');
+		}
+		else
+		{
+			$event_id = $record->event_id;
+
+			$record->assignedRoomsSlots = $this->getAssignedRoomsSlots($record->conferenceplus_session_id);
+		}
+
+		$record->rooms = $this->getRooms($event_id);
+		$record->slots = $this->getSlots($event_id);
+	}
+
+	/**
+	 * Get assigned rooms for a session
+	 *
+	 * @param   integer  $sessionId  the sessionid
+	 *
+	 * @return  mixed
+	 */
+	private function getAssignedRoomsSlots($sessionId)
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('CONCAT(room_id, "_", slot_id)')
+			->from('#__conferenceplus_sessions_to_rooms_slots')
+			->where($db->qn('session_id') . ' = ' . $db->q($sessionId));
+
+		$db->setQuery($query);
+		$result = $db->loadColumn();
+
+		return $result;
+	}
+
+	/**
+	 * Get slots for an event
+	 *
+	 * @param   integer  $eventId  the event id
+	 *
+	 * @return  mixed
+	 */
+	private function getSlots($eventId)
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('s.*,d.name as dayname')
+			->from('#__conferenceplus_slots as s')
+			->from('#__conferenceplus_days as d')
+			->where('d.conferenceplus_day_id = s.day_id')
+			->where('d.event_id' . ' = ' . $db->q($eventId))
+			->where($db->qn('s.enabled') . ' = 1')
+			->where($db->qn('d.enabled') . ' = 1')
+			->where($db->qn('s.slottype') . ' < 3')
+			->order('d.sdate ASC');
+
+		$db->setQuery($query);
+		$slots = $db->loadObjectList();
+
+		return $slots;
+	}
+
+	/**
+	 * Get rooms for an event
+	 *
+	 * @param   integer  $eventId  the event id
+	 *
+	 * @return  mixed
+	 */
+	private function getRooms($eventId)
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('*')
+			->from('#__conferenceplus_rooms')
+			->where($db->qn('event_id') . ' = ' . $db->q($eventId))
+			->where($db->qn('enabled') . ' = 1');
+
+		$db->setQuery($query);
+		$rooms = $db->loadObjectList();
+
+		return $rooms;
+	}
+
+	/**
 	 * This method runs before the $data is saved to the $table. Return false to
 	 * stop saving.
 	 *
@@ -59,37 +167,199 @@ class ConferenceplusModelSessions extends ConferenceplusModelDefault
 
 		if (FOFPlatform::getInstance()->isFrontend())
 		{
-			if ($this->_isNewRecord)
-			{
-				$event_id = JFactory::getApplication()->getUserState('com_conferenceplus.eventId');
-				$data['event_id'] = $event_id;
-
-				$speakerId = $this->checkSpeaker($data);
-
-				if ($speakerId === false)
-				{
-					$speakerId = $this->createNewSpeaker($data);
-				}
-
-				// At this point we should have a speaker id
-				$data['speaker_listids'] = $speakerId;
-			}
-			else
-			{
-				$speaker_lists = $table->speaker_listids;
-				$old_mainspeakers = explode(',', $speaker_lists);
-
-				if ($old_mainspeakers[0] != $data['mainspeaker'])
-				{
-					$old_mainspeakers[0] = $data['mainspeaker'];
-					$data['speaker_listids'] = implode(',', $old_mainspeakers);
-				}
-			}
-
-			$data['modified'] = JFactory::getDate()->toSql();
+			return $this->onBeforeSaveFrontend($data, $table);
 		}
 
 		return true;
+	}
+
+	/**
+	 * This method runs in frontend before the $data is saved to the $table. Return false to
+	 * stop saving.
+	 *
+	 * @param   array     &$data   The data to save
+	 * @param   FOFTable  &$table  The table to save the data to
+	 *
+	 * @return  boolean  Return false to prevent saving, true to allow it
+	 */
+	protected function onBeforeSaveFrontend(&$data, &$table)
+	{
+		if ($this->_isNewRecord)
+		{
+			$event_id = JFactory::getApplication()->getUserState('com_conferenceplus.eventId');
+			$data['event_id'] = $event_id;
+
+			$speakerId = $this->checkSpeaker($data);
+
+			if ($speakerId === false)
+			{
+				$speakerId = $this->createNewSpeaker($data);
+			}
+
+			// At this point we should have a speaker id
+			$data['speaker_listids'] = $speakerId;
+		}
+		else
+		{
+			$speaker_lists = $table->speaker_listids;
+			$old_mainspeakers = explode(',', $speaker_lists);
+
+			if ($old_mainspeakers[0] != $data['mainspeaker'])
+			{
+				$old_mainspeakers[0] = $data['mainspeaker'];
+				$data['speaker_listids'] = implode(',', $old_mainspeakers);
+			}
+		}
+
+		$data['modified'] = JFactory::getDate()->toSql();
+
+		return true;
+	}
+
+	/**
+	 * This method runs after the data is saved to the $table.
+	 *
+	 * @param   FOFTable  &$table  The table which was saved
+	 *
+	 * @return  boolean
+	 */
+	protected function onAfterSave(&$table)
+	{
+		if ( ! parent::onAfterSave($table))
+		{
+			return false;
+		}
+
+		if (FOFPlatform::getInstance()->isFrontend())
+		{
+			return true;
+		}
+
+		$result = $this->assignSessionsToRoomSlots($table->event_id, $table->conferenceplus_session_id);
+
+		return true;
+	}
+
+	/**
+	 * Assign a session to rooms
+	 *
+	 * @param   integer  $eventId    the id of the event data
+	 * @param   integer  $sessionId  the session id
+	 *
+	 * @return bool
+	 */
+	private function assignSessionsToRoomSlots($eventId, $sessionId)
+	{
+		$input = $this->input;
+
+		$rooms = $this->getRooms($eventId);
+		$slots = $this->getSlots($eventId);
+
+		$savedAssignedRoomsSlots = $this->getAssignedRoomsSlots($sessionId);
+
+		$assignedRoomsSlot = [];
+
+		foreach ($slots as $slot)
+		{
+			foreach ($rooms as $room)
+			{
+				$tag = 'assignment_' . $room->conferenceplus_room_id . '_' . $slot->conferenceplus_slot_id;
+
+				if ($input->get($tag, 0) != 0 )
+				{
+					$assignedRoomsSlot[] = $room->conferenceplus_room_id . '_' . $slot->conferenceplus_slot_id;
+				}
+			}
+		}
+
+		$this->assignRoomsSlots(array_diff($assignedRoomsSlot, $savedAssignedRoomsSlots), $sessionId);
+		$this->unassignRoomsSlots(array_diff($savedAssignedRoomsSlots, $assignedRoomsSlot), $sessionId);
+
+		return true;
+	}
+
+
+	/**
+	 * Assign a session to rooms/slots
+	 *
+	 * @param   array    $items      the data
+	 * @param   integer  $sessionId  the session id
+	 *
+	 * @return bool
+	 */
+	private function assignRoomsSlots($items, $sessionId)
+	{
+		if ( ! empty($items))
+		{
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+
+			$query->insert('#__conferenceplus_sessions_to_rooms_slots')
+				->columns(
+					array(
+						$db->quoteName('session_id'),
+						$db->quoteName('room_id'),
+						$db->quoteName('slot_id')
+					)
+				);
+
+			foreach ($items as $item)
+			{
+				list($room, $slot) = explode('_', $item);
+				$query->values(
+					(int) $sessionId . ', ' .
+					(int) $room . ', ' .
+					(int) $slot
+				);
+			}
+
+			$db->setQuery($query);
+
+			if ( ! $db->execute())
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Unassign a session to rooms/slots
+	 *
+	 * @param   array    $items      the data
+	 * @param   integer  $sessionId  the session id
+	 *
+	 * @return bool
+	 */
+	private function unassignRoomsSlots($items, $sessionId)
+	{
+		$result = true;
+
+		if ( ! empty($items))
+		{
+			foreach ($items AS $item)
+			{
+				list($room, $slot) = explode('_', $item);
+
+				$db = $this->getDbo();
+				$query = $db->getQuery(true);
+
+				$query->delete('#__conferenceplus_sessions_to_rooms_slots')
+					->where('session_id =' . (int) $sessionId)
+					->where('room_id = ' . (int) $room)
+					->where('slot_id = ' . (int) $slot);
+
+				$db->setQuery($query);
+
+				if ( ! $db->execute())
+				{
+					$result = false;
+				}
+			}
+		}
+
+		return $result;
 	}
 
 	/**
---
+++ code/administrator/components/com_conferenceplus/views/session/tmpl/form.php
@@ -0,0 +1,123 @@
+<?php
+ /**
+ * Conferenceplus
+ *
+ * @package    Conferenceplus
+ * @author     Robert Deutz <rdeutz@googlemail.com>
+ *
+ * @copyright  2015 JandBeyond
+ * @license    GNU General Public License version 2 or later
+ **/
+
+// No direct access
+defined('_JEXEC') or die;
+
+JHTML::_('behavior.framework', true);
+JHtml::_('behavior.formvalidator');
+JHtml::_('behavior.keepalive');
+JHtml::_('formbehavior.chosen', 'select');
+
+JFactory::getDocument()->addScriptDeclaration('
+	Joomla.submitbutton = function(task)
+	{
+		if (task == "cancel" || document.formvalidator.isValid(document.id("adminForm"))) {
+			Joomla.submitform(task, document.getElementById("adminForm"));
+		} else {
+			alert("Invalid form");
+		}
+	};
+');
+
+$fields = ['enabled','title','description','catid','sessiontype','addidionalinfo',
+	       'mainspeakerid','notes','slides','video','event_id'];
+
+$rooms = $this->item->rooms;
+$slots = $this->item->slots;
+
+$asingingPossible = !empty($rooms) && !empty($slots);
+
+if ($asingingPossible)
+{
+	$dayName = $slots[0]->dayname;
+	$allColCount = count($rooms) + 1;
+}
+
+?>
+<form id="adminForm" class="form-validate" name="adminForm" method="post" action="index.php">
+
+	<div class="form-horizontal">
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'general')); ?>
+
+			<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'general', JText::_('COM_CONFERENCEPLUS_SESSION_DATA', true)); ?>
+				<?php foreach($fields as $field) : ?>
+					<div class="row-fluid">
+						<div class="span2">
+							<?php echo $this->form->getLabel($field); ?>
+						</div>
+						<div class="span10">
+							<?php echo $this->form->getInput($field); ?>
+
+						</div>
+					</div>
+					<br />
+				<?php endforeach; ?>
+			<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+			<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'assign', JText::_('COM_CONFERENCEPLUS_ASSIGN_SESSION', true)); ?>
+				<div class="row-fluid">
+					<div class="span12">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th>
+										Slot
+									</th>
+									<?php foreach($rooms as $room) : ?>
+										<th>
+											<?php echo $room->name; ?>
+										</th>
+									<?php endforeach; ?>
+								</tr>
+							</thead>
+							<tbody>
+								<tr style="background-color: #004099;color:#fff;font-weight: bold"><td colspan="<?php echo $allColCount; ?>"><?php echo $dayName; ?></td></tr>
+								<?php foreach($slots as $slot) : ?>
+									<?php if ($slot->dayname != $dayName) : ?>
+										<?php $dayName = $slot->dayname; ?>
+										<tr style="background-color: #004099;color:#fff;font-weight: bold"><td colspan="<?php echo $allColCount; ?>"><?php echo $dayName; ?></td></tr>
+									<?php endif; ?>
+									<tr>
+										<td>
+											<?php echo $slot->name; ?>
+											&nbsp;(<?php echo substr($slot->stime, 0, 5); ?> - <?php echo substr($slot->etime, 0,5); ?>)
+										</td>
+										<?php foreach($rooms as $room) : ?>
+											<td>
+												<?php
+													$id = $room->conferenceplus_room_id . '_' . $slot->conferenceplus_slot_id;
+													$checked = '';
+													if (in_array($id, $this->item->assignedRoomsSlots))
+													{
+														$checked = ' checked="checked"';
+													}
+												?>
+												<input type="checkbox" name="<?php echo 'assignment_' . $id; ?>" value="1"<?php echo $checked; ?>>
+											</td>
+										<?php endforeach; ?>
+									</tr>
+								<?php endforeach; ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			<?php echo JHtml::_('bootstrap.endTab'); ?>
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+	</div>
+
+	<input type="hidden" value="com_conferenceplus" name="option">
+	<input type="hidden" value="session" name="view">
+	<input type="hidden" value="" name="task">
+	<input type="hidden" value="<?php echo $this->item->conferenceplus_session_id; ?>" name="conferenceplus_session_id">
+	<?php echo JHtml::_('form.token'); ?>
+
+</form>
