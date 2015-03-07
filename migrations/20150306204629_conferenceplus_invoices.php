9eae1e13eeb2e0d0911ddb90352300c5652857e2
Robert Deutz <rdeutz@googlemail.com>
2015-03-07 21:19:39 +0100

    adding migration for the invoices table

---
+++ migrations/20150306204629_conferenceplus_invoices.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ConferenceplusInvoices extends AbstractMigration
+{
+    protected $tableName = 'conferenceplus_invoices';
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        // create the table
+        $table = $this->table($this->tableName, ['id' => 'conferenceplus_invoice_id']);
+        $table->addColumn('payment_id', 'integer', ['comment' => 'relation to a payment'])
+            ->addColumn('identifier', 'string' , ['comment' => 'a uniq id for the invoice'])
+            ->addColumn('data', 'text' , ['comment' => 'data for the invoice'])
+            ->addColumn('created', 'datetime', ['comment' => 'Entry created'])
+            ->addColumn('modified', 'datetime', ['comment' => 'Last modification'])
+            ->addColumn('enabled', 'integer', ['comment' => 'makes it available'])
+            ->addIndex(['conferenceplus_invoice_id'], ['unique' => true])
+            ->addIndex(['identifier'], ['unique' => true])
+            ->create();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
