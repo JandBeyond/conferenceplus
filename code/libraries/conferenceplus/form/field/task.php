8420922eeb176272b97e1a421526d7f16b3dbf03
Robert Deutz <rdeutz@googlemail.com>
2015-03-28 11:17:20 +0100

    adding task form field

---
+++ code/libraries/conferenceplus/form/field/task.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * conferenceplus
+ * @author Robert Deutz <rdeutz@googlemail.com>
+ * @package conferenceplus
+ **/
+
+defined('JPATH_BASE') or die;
+
+JFormHelper::loadFieldClass('list');
+
+/**
+ * field type
+ *
+ * @package  conferenceplus
+ * @since    1.0.0
+ */
+class ConferenceplusFormFieldTask extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 */
+	protected $type = 'Task';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return array
+	 */
+	public function getOptions()
+	{
+		$options = array();
+
+		$options[] = JHtml::_('select.option', 0, '- Select -');
+		$options[] = JHtml::_('select.option', 'AfterPayment', 'AfterPayment');
+		$options[] = JHtml::_('select.option', 'ConfirmEmail', 'ConfirmEmail');
+		$options[] = JHtml::_('select.option', 'CreateAttendees', 'CreateAttendees');
+		$options[] = JHtml::_('select.option', 'InvoiceHandling', 'InvoiceHandling');
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+}
