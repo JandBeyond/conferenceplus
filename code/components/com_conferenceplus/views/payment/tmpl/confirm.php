0637759cb850ed69b3e4eff1121f30575190ef03
Stefan Wendhausen <sw@tec-promotion.de>
2015-04-08 15:40:01 +0200

    added language string to view payment on confirm view to share on twitter

--- code/components/com_conferenceplus/views/payment/tmpl/confirm.php
+++ code/components/com_conferenceplus/views/payment/tmpl/confirm.php
@@ -18,13 +18,13 @@ if ($freeticket)
 {
 	$title = JText::_('COM_CONFERENCEPLUS_CONFIRM_GOT_TICKET_TITLE');
 	$msg   = JText::_('COM_CONFERENCEPLUS_TICKET_CONFIRM_GOT_TICKET');
-	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20have%20gotton%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">Share on Twitter with your friends</a>';
+	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20have%20gotton%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
 }
 else
 {
 	$title = JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TITLE');
 	$msg   = JText::_('COM_CONFERENCEPLUS_TICKET_CONFIRM_BUY_TICKET');
-	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20just%20bought%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">Share on Twitter with your friends</a>';
+	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20just%20bought%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
 }
 
 $doc = JFactory::getDocument()->setTitle($title);
