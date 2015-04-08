134c96e7558468bd10bc30f8b98556ee2ea8eb7c
Stefan Wendhausen <sw@tec-promotion.de>
2015-04-08 22:01:44 +0200

    add tweet message to config

--- code/components/com_conferenceplus/views/payment/tmpl/confirm.php
+++ code/components/com_conferenceplus/views/payment/tmpl/confirm.php
@@ -11,6 +11,8 @@ defined('_JEXEC') or die;
 $displayData 	= new stdClass;
 $params 		= JComponentHelper::getParams('com_conferenceplus');
 $headerlevel    = $params->get('headerlevel', 2);
+$tweetgot    = $params->get('tweetgot');
+$tweetbuy   = $params->get('tweetbuy');
 
 $freeticket = $this->input->get('ft', 0) == 1;
 
@@ -18,13 +20,13 @@ if ($freeticket)
 {
 	$title = JText::_('COM_CONFERENCEPLUS_CONFIRM_GOT_TICKET_TITLE');
 	$msg   = JText::_('COM_CONFERENCEPLUS_TICKET_CONFIRM_GOT_TICKET');
-	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20have%20gotton%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
+	$tweet = '<a target="_blank" href="https://twitter.com/home?status=' . $tweetgot . '">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
 }
 else
 {
 	$title = JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TITLE');
 	$msg   = JText::_('COM_CONFERENCEPLUS_TICKET_CONFIRM_BUY_TICKET');
-	$tweet = '<a target="_blank" href="https://twitter.com/home?status=I%20just%20bought%20my%20ticket%20for%20JandBeyond%2029-31%20May%20in%20Prague.%20See%20you%20there!%20%23joomla%20%23jab15%20http://jandbeyond.org">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
+	$tweet = '<a target="_blank" href="https://twitter.com/home?status=' . $tweetbuy . '">'. JText::_('COM_CONFERENCEPLUS_CONFIRM_BUY_TICKET_TWEET') . '</a>';
 }
 
 $doc = JFactory::getDocument()->setTitle($title);
