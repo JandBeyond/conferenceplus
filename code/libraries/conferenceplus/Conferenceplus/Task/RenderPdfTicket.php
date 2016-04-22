e95e9069e99e78f65ce1756580f62d478e0ed9e1
Robert Deutz <rdeutz@googlemail.com>
2016-04-22 21:58:01 +0200

    removed hardcoded event prefix

--- code/libraries/conferenceplus/Conferenceplus/Task/RenderPdfTicket.php
+++ code/libraries/conferenceplus/Conferenceplus/Task/RenderPdfTicket.php
@@ -47,9 +47,20 @@ class RenderPdfTicket extends RenderPdf
 		$ticket     = $task->processdata['processdata']['ticket']['ticket'];
 		$tickettype = $task->processdata['processdata']['ticket']['tickettype'];
 
+		// That's needed to find the correct table
+		$config['input']['option'] = 'com_conferenceplus';
+		$eventTable = \FOFTable::getAnInstance('event', 'JTable', $config);
+
+		if ( ! $eventTable->load($tickettype['event_id']))
+		{
+			return false;
+		}
+
+		$name    = $eventTable->name;
+
 		$data['name'] = $ticket['firstname'] . ' ' . $ticket['lastname'];
 
-		$data['ticket_number'] = 'JD15DE-' . str_pad($ticket['conferenceplus_ticket_id'], 8, '0', STR_PAD_LEFT);
+		$data['ticket_number'] = $name . '-' . str_pad($ticket['conferenceplus_ticket_id'], 8, '0', STR_PAD_LEFT);
 		$data['ticket_type']   = $tickettype['name'];
 		$data['food']          = \JText::_($ticket['processdata']['ask4food']);
 		$data['tshirtsize']    = $ticket['processdata']['ask4tshirtsize'];
